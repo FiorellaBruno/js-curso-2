6b3f0ec734d34de25370627dd3c1311762205955
Fiorella C. Bruno <61298588+FiorellaBruno@users.noreply.github.com>
2024-06-25 15:50:11 -0300

    Add feature
    Ensure all buttons are now functional

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
+let titulo = document.querySelector('h1');
+titulo.innerHTML =  " Hora del desafío ";
 
+function consoleButton(){
+    console.log("El botón fue clicado");
+}
+
+function showPrompt(){
+    let ciudadBr = prompt ("Ingrese por favor una ciudad de Brasil");
+    alert("Estuve en " + ciudadBr + " y me acordé de ti");
+}
+
+function showAlert() {
+    alert ("Amo JS");
+}
+
+function sumOfTwo(){
+    let firstNum = parseInt(prompt("Por favor, ingrese el primer num"));
+    let secondNum = parseInt(prompt("Por favor, ingrese el segundo num"));
+
+    let resultOfTwo = firstNum + secondNum;
+    alert(`${firstNum} + ${secondNum} = ${resultOfTwo}`);
+        
+    }
